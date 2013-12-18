fafb8d05acd31d1d7b7d46de9e327122d92744b8
jacknoble <jack.noble@gmail.com>
2013-12-17 18:13:02 -0800

    continue params

--- light_rail/lib/rails_lite/params.rb
+++ light_rail/lib/rails_lite/params.rb
@@ -2,7 +2,15 @@ require 'uri'
 
 class Params
   def initialize(req, route_params)
-    @params = URI.decode_www_form(req.query_string)
+    @params ||= Hash.new
+    unless req.query_string.nil?
+      @params.merge(URI.decode_www_form(req.query_string))
+    end
+
+    unless req.body.nil?
+      parsed_body = parse_www_encoded(req.body)
+      @params.merge(parsed_body)
+    end
   end
 
   def [](key)
@@ -14,9 +22,23 @@ class Params
 
   private
   def parse_www_encoded(www_encoded_form)
-    decode_www_form(www_encoded_form)
+    decoded_body = URI.decode_www_form(www_encoded_form)
+    body_hash = Hash.new
+    decoded_body.split(", ").each do |pair|
+      hash = create_body_hash(parse_key(pair.flatten!))
+      p hash
+      body_hash.merge(hash)
+    end
   end
 
-  def parse_key(key)
+  def create_body_hash(pair)
+    return pair if pair.count == 1
+    {pair[0] => create_body_hash(pair[1..-1])}
   end
+
+
+  def parse_key(key)
+    key.split(/\]\[|\[|\]/)
+  end
+
 end
